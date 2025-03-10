427ab17636d6ad2a4d0f60b787187bac82f92a5b
MelodicDrumstep <1724662527@qq.com>
2025-03-10 18:50:26 +0800

    Working on lab0: byte stream reader / writer. This commit node CAN NOT compile. I firstly want to use two ring buffers to implenement that, but the Reader's peek interface makes it so hard. I'm considering refactoring.

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -4,74 +4,84 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) 
+  : capacity_( capacity ), ring_buffers_{ RingBuffer(capacity + 1), RingBuffer(capacity + 1) }
+{}
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
+  auto & ring_buffer = ring_buffers_[current_ring_buffer_index_];
+  uint32_t data_length_can_be_pushed = std::min(static_cast<uint32_t>(data.size()), ring_buffer.availableSize());
+  bytes_pushed_ += data_length_can_be_pushed;
+  ring_buffer.push(data, data_length_can_be_pushed);
 }
 
 void Writer::close()
 {
   // Your code here.
+  flags_ &= EOF_FLAG;
 }
 
 void Writer::set_error()
 {
   // Your code here.
+  flags_ &= ERROR_FLAG;
 }
 
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return (flags_ & EOF_FLAG);
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return ring_buffers_[current_ring_buffer_index_].availableSize();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  auto & old_ring_buffer = ring_buffers_[current_ring_buffer_index_];
+  auto & new_ring_buffer = ring_buffers_[1 - current_ring_buffer_index_];
+  old_ring_buffer.copyAndRestructure(new_ring_buffer);
+  current_ring_buffer_index_ = 1 - current_ring_buffer_index_;
+  return new_ring_buffer.toStringView();
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return (flags_ & EOF_FLAG) && (ring_buffers_[current_ring_buffer_index_].isEmpty());
 }
 
 bool Reader::has_error() const
 {
   // Your code here.
-  return {};
+  return (flags_ & ERROR_FLAG);
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  auto & ring_buffer = ring_buffers_[current_ring_buffer_index_];
+  ring_buffer.read_index_ = (ring_buffer.read_index_ + len) % ring_buffer.bufferSize(); 
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return ring_buffers_[current_ring_buffer_index_].occupiedSize();
 }
 
 uint64_t Reader::bytes_popped() const
 {
   // Your code here.
-  return {};
+  return bytes_poped_;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -4,7 +4,9 @@
 #include <stdexcept>
 #include <cstdint>
 #include <string>
+#include <array>
 #include <string_view>
+#include <cstring>
 
 class Reader;
 class Writer;
@@ -14,6 +16,82 @@ class ByteStream
 protected:
   uint64_t capacity_;
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
+  
+  int8_t flags_ = 0;
+  uint64_t bytes_pushed_ = 0;
+  uint64_t bytes_poped_ = 0;
+  constexpr static uint32_t EOF_FLAG = 0b01;
+  constexpr static uint32_t ERROR_FLAG = 0b10;
+
+  struct RingBuffer
+  {
+    RingBuffer(const uint32_t buffer_size) : buffer_size_(buffer_size), buffer_(buffer_size) {}
+    RingBuffer(const RingBuffer &) = delete;
+    RingBuffer(RingBuffer &&) = delete;
+    RingBuffer & operator=(const RingBuffer &) = delete;
+    RingBuffer & operator=(RingBuffer &&) = delete;
+
+    uint32_t capacity() const {
+      return buffer_size_ - 1;
+    }
+    uint32_t bufferSize() const {
+      return buffer_size_;
+    }
+    uint32_t occupiedSize() const {
+      return (write_index_ + buffer_size_ - read_index_) % buffer_size_; 
+    }
+    uint32_t availableSize() const {
+      return (capacity() - occupiedSize());
+    }
+    uint32_t availableWriteSizeWithoutLoopingBack() const {
+      return buffer_size_ - write_index_;
+    }
+    bool isEmpty() const {
+      return (write_index_ == read_index_);
+    }
+    bool isFull() const {
+      return (occupiedSize() == capacity());
+    }
+    char top() {
+      return buffer_[read_index_];
+    }
+    void pop() {
+      read_index_++;
+    }
+    void push(const std::string & data, uint32_t len) {
+      uint32_t data_length_pushed_directly_without_looping_back = std::min(len, availableWriteSizeWithoutLoopingBack());
+      std::memcpy(&(buffer_[write_index_]), data.c_str(), data_length_pushed_directly_without_looping_back);
+      if(data_length_pushed_directly_without_looping_back < len) {
+        std::memcpy(&(buffer_[0]), data.c_str() + data_length_pushed_directly_without_looping_back, len - data_length_pushed_directly_without_looping_back);
+      }
+      write_index_ = (write_index_ + len) % bufferSize();
+    }
+
+    void copyAndRestructure(RingBuffer & other) const {
+      if(read_index_ < write_index_) {
+        std::memcpy(&(other.buffer_[0]), &(buffer_[read_index_]), write_index_ - read_index_);
+      } else if(write_index_ < read_index_) {
+        uint32_t read_index_tail_distance = buffer_size_ - read_index_;
+        std::memcpy(&(other.buffer_[0]), &(buffer_[read_index_]), read_index_tail_distance);
+        std::memcpy(&(other.buffer_[read_index_tail_distance]), &(buffer_[0]), write_index_);
+      }
+
+      other.write_index_ = 0;
+      other.read_index_ = (write_index_ + buffer_size_ - read_index_) % buffer_size_;
+    }
+
+    std::string_view toStringView() const {
+      return std::string_view(&(buffer_[0]), occupiedSize());
+    }
+
+    uint32_t buffer_size_;
+    mutable std::vector<char> buffer_;
+    uint32_t write_index_ = 0;
+    uint32_t read_index_ = 0;
+  };
+
+  std::array<RingBuffer, 2> ring_buffers_;
+  uint8_t current_ring_buffer_index_ = 0;
 
 public:
   explicit ByteStream( uint64_t capacity );
