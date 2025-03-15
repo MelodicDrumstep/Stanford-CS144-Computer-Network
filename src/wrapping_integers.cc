4a07f002499ad969070972f996754a1077ad23a1
MelodicDrumstep <1724662527@qq.com>
2025-03-15 23:06:11 +0800

    Implement Wrap32 for TCP receiver. Pass the tests.

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,7 +1,4 @@
-
-// TODO: Delete this after testing
-#include <iostream>
-#include <cmath>
+#include <limits>
 
 #include "wrapping_integers.hh"
 
@@ -16,27 +13,18 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  constexpr uint64_t TwoPower32 = 1ULL << 32;
-  uint32_t zero_point_raw_value = zero_point.getRawValue();
+  // Referenced from https://github.com/comzyh/TCP-Lab/blob/comzyh/libsponge/wrapping_integers.cc
+  // This method is awesome
+  Wrap32 wrap_checkpoint = wrap(checkpoint, zero_point);
+  uint32_t diff = raw_value_ - wrap_checkpoint.getRawValue();
 
-  // DEBUGING
-  std::cout << "[Wrap32::unwrap] raw_value_ is " << raw_value_ << ", zero_point_raw_value is " << zero_point_raw_value << "\n";
-  std::cout << "raw_value_ - zero_point_raw_value is " << raw_value_ - zero_point_raw_value << "\n";
-  std::cout << "TwoPower32 is " << TwoPower32 << std::endl;
-  std::cout << "checkpoint + zero_point_raw_value - raw_value_ is " << checkpoint + zero_point_raw_value - raw_value_ << "\n";
-  std::cout << "(checkpoint + zero_point_raw_value - raw_value_ + TwoPower32) is " << (checkpoint + zero_point_raw_value - raw_value_ + TwoPower32) << "\n";
-  std::cout << "(checkpoint + zero_point_raw_value - raw_value_ + TwoPower32) / TwoPower32 is " << (checkpoint + zero_point_raw_value - raw_value_ + TwoPower32) / TwoPower32 << "\n";
-  // DEBUGING
-
-  uint64_t helper = (zero_point_raw_value > raw_value_) ? zero_point_raw_value - raw_value_ : raw_value_ - zero_point_raw_value;
-  uint64_t n = (checkpoint - helper + TwoPower32) / TwoPower32;
-  uint64_t upper_bound_ans = raw_value_ - zero_point_raw_value + TwoPower32 * n;
-  uint64_t lower_bound_ans = raw_value_ - zero_point_raw_value + TwoPower32 * (n - 1);
-
-  std::cout << "checkpoint is " << checkpoint << "\n";
-  std::cout << "n is " << n << "\n";
-  std::cout << "upper_bound_ans is " << upper_bound_ans << "\n";
-  std::cout << "lower_bound_ans is " << lower_bound_ans << "\n";
-
-  return ((n == 0) || ((upper_bound_ans - checkpoint) <= (checkpoint - lower_bound_ans))) ? upper_bound_ans : lower_bound_ans;
+  if (diff & 0x80000000 && diff + checkpoint <= std::numeric_limits<uint32_t>::max()) {
+    // diff & 0x80000000 means the highest bit for diff is 1. 
+    // Therefore it's negative if converted to int32_t
+    // In this situation, we cannot direcly return checkpoint + static_cast<int32_t>(diff)
+    // otherwise the result would overflow
+    // We should use diff as uint32_t here
+    return checkpoint + diff;
+  }
+  return checkpoint + static_cast<int32_t>(diff);
 }
